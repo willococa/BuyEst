8fa5fb2869e384307403b2659be2897a3722cb55
willocoro <willocoro25@gmail.com>
2023-05-18 02:14:18 -0500

    added products and images for it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create 4 Devise clients
+4.times do |i|
+    Client.create!(
+      email: "client#{i+1}@example.com",
+      password: 'password',
+      password_confirmation: 'password'
+      # Add additional fields as necessary
+      # Example: name: "Client #{i+1}", age: 30, address: "123 Example St"
+    )
+  end
